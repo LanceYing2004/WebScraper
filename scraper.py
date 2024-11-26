c05439777d8985884e5cad6553ae7baeef02ac5c
Jim Xiong <xiong.jim131@gmail.com>
2024-11-26 12:26:35 -0500

    scraper main

--- scraper.py
+++ scraper.py
@@ -1,11 +1,15 @@
 from utils import scrape_conf
 import pandas as pd
 from scrape_acm import scrape_acm
+# from scrape_IEEE import scrape_IEEE
 
 all_papers = []
-all_papers += scrape_conf("chi", scrape_acm)
-all_papers += scrape_conf("sp")
+conf = [("chi", scrape_acm)]
+# conf += ("sp", scrape_IEEE)
+for c, scraper in conf:
+    all_papers += scrape_conf(c, scraper)
+# all_papers += scrape_conf("sp", scrape_IEEE)
 
 papers_df = pd.DataFrame(all_papers, columns=["venue", "year", "DOI", "bibtext"])
 
-print(papers_df)
+papers_df.to_csv('papers.csv', index=False)
